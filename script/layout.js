e5952e8ca3d72963198f680732a8da7aa3f1b20f
omar osama <omarosama200100@gmail.com>
2016-11-23 17:28:48 -0800

    new changes

--- script/layout.js
+++ script/layout.js
@@ -1,52 +1,30 @@
 var radialLayout = function () {
 
-    var minRange = 0;
-    var maxRange = 24;
     var gmap = d3.map();
-    var label ;
-    var labelHasSet = false;
 
-    var value = function (d) {
-        return d;
-    };
+    var maxRange = 20;
 
     //layout algorithm
     var layout = function (data) {
-        var range = d3.range(minRange, maxRange);
-        if(!labelHasSet){
-            label = range;
-        }
+        var range = d3.range(0, maxRange);
         var grouped = [];
 
         //transform and returns the grouped data
 
         range.forEach(function (r) {
-            gmap.set(r, {range: label[r], startAngle: 0, endAngle: 0, count: data[r]});
+            gmap.set(r, {startAngle: 0, endAngle: 0, count: data[r]});
         });
 
 
         grouped = gmap.values();
-        grouped.forEach(function (d, i) {
+        grouped.forEach(function (r, i) {
             var itemAngle = Math.PI * 2 / maxRange;
-            d.startAngle = itemAngle * i;
-            d.endAngle = itemAngle * (i + 1);
+            r.startAngle = itemAngle * i;
+            r.endAngle = itemAngle * (i + 1);
         });
         
 
         return grouped;
-    };
-
-    layout.value = function (accessorfunction) {
-        if(!arguments.length) return value;
-
-        value = accessorfunction;
-        return layout;
-    };
-
-    layout.setLabel = function(l){
-        if(!arguments.length) return label;
-        label = l;
-        return layout;
     };
 
     return layout;
--- script/useLayout.js
+++ script/useLayout.js
@@ -1,52 +1,35 @@
-//	Generate	a	dataset	with	random	dates.
-var data = addData([], 300, 20 * 60);
-//	Get	the	layout	function.
-var layout = radialLayout();
-//	Compute	the	ouput	data.
-var hours = [];
-var hourscount = [];
-data.forEach(function (d, i) {
-    hours[i] = d.date.getHours();
-});
+// //	Generate	a	dataset	with	random	dates.
+var data = [10,20,50,55,21,32,12,10,20,50,55,21,32,12,10,20,50,55,21,32];
 
+// //	Get	the	layout	function.
+ var layout = radialLayout();
 
-var countmap = d3.range(0, 24);
-countmap.forEach(function (d) {
-    var count = 0;
-    for (var i = 0; i < hours.length; i++) {
-        if (hours[i] == d) {
-            count++
-        }
-    }
-    hourscount[d] = count;
-});
+var output = layout(data);
 
-
-var output = layout(hourscount);
 
 var width = 1000,
     height = 600,
     innerRadius = 30,
-    outerRadius = 250;
-
-var rScale = d3.scaleSqrt()
-    .domain([0, d3.max(output, function (d) {
+    outerRadius = 200;
+//
+var rScale = d3.scaleLinear()
+    .domain([0, d3.max(output , function (d) {
         return d.count;
     })])
     .range([0, outerRadius - innerRadius]);
-
+//
 var colorScale = d3.scaleLinear()
     .domain([0, d3.max(output, function (d) {
         return d.count;
     })])
     .range(["#ffeda0", "#f03b20"]);
-
+//
 var arc = d3.arc()
     .innerRadius(innerRadius)
     .outerRadius(function (d) {
         return innerRadius + rScale(d.count);
     });
-
+//
 var labelArc = d3.arc()
     .innerRadius(function (d) {
         return innerRadius + rScale(d.count) + 10;
@@ -76,6 +59,8 @@ var arcs = groups.append("path")
     .attr("stroke", "white")
     .attr("stroke-width", 1);
 
+
+////////////////////////////////////////////////////////////////////////////////////
 var labels = groups.append("text")
     .attr("transform", function (d) {
         return "translate(" + labelArc.centroid(d) + ")rotate(" + angle(d) + ")";
@@ -87,21 +72,6 @@ var labels = groups.append("text")
         return d.count;
     });
 
-function randomInterval(avgSeconds) {
-    return Math.floor(-Math.log(Math.random()) * 1000 * avgSeconds);
-};
-
-function addData(data, numItems, avgSeconds) {
-//	Compute	the	most	recent	time	in	the	data	array.
-    var n = data.length,
-        t = (n > 0) ? data[n - 1].date : new Date();
-//	Append	items	with	increasing	times	in	the	data	array.
-    for (var k = 0; k < numItems - 1; k += 1) {
-        t = new Date(t.getTime() + randomInterval(avgSeconds));
-        data.push({date: t});
-    }
-    return data;
-};
 
 function angle(d) {
     var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
